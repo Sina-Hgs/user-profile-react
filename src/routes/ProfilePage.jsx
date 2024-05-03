caa8668b68a1628d0346aa8389c6562fe7b8e5d2
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2024-05-03 19:01:56 +0330

    made the uselocalStorage custom hook

--- src/routes/ProfilePage.jsx
+++ src/routes/ProfilePage.jsx
@@ -1,5 +1,13 @@
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <div>
+      <h1>Profile</h1>
+      <h2>Sina</h2>
+      <p>myemail@gmail.com</p>
+      <p>Male</p>
+      <p>01/01/2000</p>
+    </div>
+  );
 };
 
 export default ProfilePage;
